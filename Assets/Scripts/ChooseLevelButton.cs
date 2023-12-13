2eeac907aebbe47fc46ca059809fe419a951e038
RadiALo <78644882+RadiALo@users.noreply.github.com>
2023-12-13 04:29:17 +0200

    Main Menu

---
+++ Assets/Scripts/ChooseLevelButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseLevelButton : MonoBehaviour
+{
+    [SerializeField]
+    private int level;
+
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+
+    }
+}
