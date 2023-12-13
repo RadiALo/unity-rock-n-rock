177c103fbfb181efa7d26329cee57abdba8bd48d
RadiALo <78644882+RadiALo@users.noreply.github.com>
2023-12-13 16:14:52 +0200

    Better collision layers

--- Assets/Scripts/Battle/AttackZone.cs
+++ Assets/Scripts/Battle/AttackZone.cs
@@ -7,6 +7,8 @@ public class AttackZone : MonoBehaviour
     [SerializeField]
     private int damage = 1;
 
+    public int Damage { get => 1; }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
